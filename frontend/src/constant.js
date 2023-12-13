cf51cc93b284c1daeac68b93cb06b7bc53c59f8a
sheelaadhikari <adhikarisheela1000@gmail.com>
2023-12-13 10:38:10 +0545

    tested all

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,4 +1,3 @@
 
 
 export const BASEURL = 'http://localhost:9000';
-export const URL = 'http://localhost:3000';
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,97 +1,88 @@
 import React, { useState } from "react";
-import '../App.css';
-
-
+import "../App.css";
 
 import axios from "axios";
-import { useLocation, useNavigate, } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 // import { useAuth } from './../context/auth';
-import { BASEURL } from './../constant';
+import { BASEURL } from "./../constant";
 const HomePage = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    // const [auth, setAuth] = useAuth;
-    const navigate = useNavigate();
-    const location = useLocation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const [auth, setAuth] = useAuth;
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    //  form function
+  //  form function
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`${BASEURL}/api/v1/auth/login`, {
+        email,
+        password,
+      });
+      console.log(res.data);
+      if (res.data.success) {
+        console.log(res.data.message);
+        // setAuth({
+        //     ...auth,
+        //     user: res.data.user,
+        //     token: res.data.token,
+        // });
+        localStorage.setItem("auth", JSON.stringify(res.data));
+        axios.defaults.headers.common["Authorization"] = res.data.token;
+        navigate(location.state || "/Upload");
+      } else {
+        console.log(" user not found", res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      console.log("something went wrong");
+    }
+  };
 
+  return (
+    <div className="form">
+      <div className="form-wrapper">
+        <form onSubmit={handleSubmit}>
+          <h4 className="title">Login Admin</h4>
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const res = await axios.post(`${BASEURL}/api/v1/auth/login`, { email, password });
-            console.log(res.data);
-            if (res.data.success) {
-                console.log(res.data.message);
-                // setAuth({
-                //     ...auth,
-                //     user: res.data.user,
-                //     token: res.data.token,
-                // });
-                localStorage.setItem("auth", JSON.stringify(res.data));
-                axios.defaults.headers.common["Authorization"] = res.data.token;
-                navigate(location.state || "/Upload");
-            } else {
+          <div className="mb-3">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              className="form-control"
+              id="exampleInputEmail"
+              aria-describedby="emailHelp"
+              placeholder=" Enter Your Email"
+              required
+            />
+          </div>
 
-                console.log(" uset not found", res.data.message);
+          <div className="mb-3">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              className="form-control"
+              id="exampleInputPassword"
+              placeholder="Enter Your Password"
+              required
+            />
+          </div>
 
-            }
-        } catch (error) {
-            console.log(error);
-            console.log("something went wrong");
-        }
-    };
-
-    return (
-
-        <div className="form">
-            <div className="form-wrapper">
-                <form onSubmit={handleSubmit}>
-                    <h4 className="title">Login Admin</h4>
-
-                    <div className="mb-3">
-                        <input
-                            type="email"
-                            value={email}
-                            onChange={(e) => {
-                                setEmail(e.target.value);
-                            }}
-                            className="form-control"
-                            id="exampleInputEmail"
-                            aria-describedby="emailHelp"
-                            placeholder=" Enter Your Email"
-                            required
-                        />
-                    </div>
-
-
-                    <div className="mb-3">
-                        <input
-                            type="password"
-                            value={password}
-                            onChange={(e) => {
-                                setPassword(e.target.value);
-                            }}
-                            className="form-control"
-                            id="exampleInputPassword"
-                            placeholder="Enter Your Password"
-                            required
-                        />
-                    </div>
-
-                    <button type="submit" className="click" >
-                        Submit
-                    </button>
-                </form>
-
-            </div>
-
-        </div>
-
-
-    );
+          <button type="submit" className="click">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
